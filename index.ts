14342e700b28c9c0ddd11ff7c9b46691394431d2
CarlosDRomero <cdavid.romeror@gmail.com>
2025-11-20 14:39:28 -0500

    fix cors policy error

--- index.ts
+++ index.ts
@@ -3,12 +3,19 @@ import express, { Request, Response, Router } from "express";
 import responseTime from 'response-time'
 import client from "./cache/redis-client";
 import searchRouter from "./routes/search";
+import cors from "cors"
+
+const corsOptions = {
+  origin: process.env.FRONTEND_ORIGIN,
+  optionsSuccessStatus: 200
+};
 
 const app = express();
 const PORT = 5000;
 
 
 app.use(responseTime())
+app.use(cors(corsOptions));
 app.use("/search",searchRouter)
 app.get("/health", (req: Request, res: Response) => {
   res.json({ message: "Hello World" });
